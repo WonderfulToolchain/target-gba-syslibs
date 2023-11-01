75a73c707c7023566646d3f5ce19cf3609ae6622
Adrian Siekierka <kontakt@asie.pl>
2023-11-01 18:57:25 +0100

    libgba, target: fix sbrk()

--- libgba/src/libc/sbrk.c
+++ libgba/src/libc/sbrk.c
@@ -8,25 +8,20 @@
 #include <stdint.h>
 
 void *sbrk(int incr) {
-    // Symbol defined by the linker
-    extern char __end__[];
-    const uintptr_t end = (uintptr_t)__end__;
-
-    // Trick to get the current stack pointer
-    register uintptr_t stack_ptr asm("sp");
+    extern char __eheap_start[];
+    const uintptr_t eheap_start = (uintptr_t)__eheap_start;
+    extern char __eheap_end[];
+    const uintptr_t eheap_end = (uintptr_t)__eheap_end;
 
     // Next address to be used. It is updated after every call to sbrk()
     static uintptr_t heap_start = 0;
 
     if (heap_start == 0) {
-        heap_start = end;
+        heap_start = eheap_start;
     }
 
-    // Current limit
-    uintptr_t heap_end = stack_ptr;
-
     // Try to allocate
-    if (heap_start + incr > heap_end) {
+    if (heap_start + incr > eheap_end) {
         errno = ENOMEM;
         return (void *)-1;
     }
